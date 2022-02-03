18bd32e62194b1a4babfa4f622657b283c174440
Spec27 <shkvara27@gmail.com>
2022-02-03 19:49:23 +0200

    Сделал задачу 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const PROMPT_DELAY = 1000;
+
+const startChangeColorBtn = document.querySelector('button[data-start]');
+const stopChangeColorBtn = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
+
+startChangeColorBtn.addEventListener('click', changeColor);
+
+function changeColor() {
+  startChangeColorBtn.disabled = true;
+  timer = setInterval(() => {
+    bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+  }, PROMPT_DELAY);
+}
+
+stopChangeColorBtn.addEventListener('click', () => {
+  clearInterval(timer);
+  startChangeColorBtn.disabled = false;
+});
